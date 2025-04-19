ce8f281e2edbf6bbfbef13aa0234412236bd3be8
falk022 <hussainfarish2005@gmail.com>
2025-04-19 18:50:14 +0500

    Tweaks to the Admin Dashboard

--- resources/views/admin/footer.blade.php
+++ resources/views/admin/footer.blade.php
@@ -3,6 +3,7 @@
             <div class="container-fluid text-center">
               <!-- Please do not remove the backlink to us unless you support us at https://bootstrapious.com/donate. It is part of the license conditions. Thank you for understanding :)-->
                <p class="no-margin-bottom">2018 &copy; Your company. Download From <a target="_blank" href="https://templateshub.net">Templates Hub</a>.</p>
+               <p>Edited By Hussain Farish Ismail S2102326</p>
             </div>
           </div>
         </footer>
--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -3,7 +3,7 @@
       <nav id="sidebar">
         <!-- Sidebar Header-->
         <div class="sidebar-header d-flex align-items-center">
-          <div class="avatar"><img src="admin/img/avatar-12.png" alt="..." class="img-fluid rounded-circle"></div>
+          <!-- <div class="avatar"><img src="admin/img/avatar-12.png" alt="..." class="img-fluid rounded-circle"></div> -->
           <div class="title">
             <h1 class="h5">Hussain Farish Ismail</h1>
             <p>Student</p>
@@ -12,7 +12,7 @@
         </div>
         <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
         <ul class="list-unstyled">
-                <li class="active"><a href="index.html"> <i class="icon-home"></i>Home </a></li>
+                <li class="active"><a href="{{url('/home')}}"> <i class="icon-home"></i>Home </a></li>
                
                 <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-windows"></i>Hotel Rooms </a>
                   <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
